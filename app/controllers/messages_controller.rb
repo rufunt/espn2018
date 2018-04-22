96dedc9636540ff29cc67d615c4e7703bdac3191
rufunt <jazzoo@mail.ru>
2018-04-22 21:19:23 +0300

    Add edit action and view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
     @messages = Message.all.order("created_at DESC")
@@ -9,7 +10,6 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def create
@@ -22,10 +22,30 @@ class MessagesController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    
+  end
+
   private
 
   def message_params
     params.require(:message).permit(:title, :description)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
   end
   
 end
